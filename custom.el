b3cf867234eb51414855af8eb1c0071797fc5a9a
Bryan Maass <bryan.maass@gmail.com>
2015-07-03 23:45:16 +1000

    moew theme + powerline

--- custom.el
+++ custom.el
@@ -5,10 +5,20 @@
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
    (quote
-    ("f0a99f53cbf7b004ba0c1760aa14fd70f2eabafe4e62a2b3cf5cabae8203113b" "9dae95cdbed1505d45322ef8b5aa90ccb6cb59e0ff26fef0b8f411dfc416c552" "bb08c73af94ee74453c90422485b29e5643b73b05e8de029a6909af6a3fb3f58" "628278136f88aa1a151bb2d6c8a86bf2b7631fbea5f0f76cba2a0079cd910f7d" "1b8d67b43ff1723960eb5e0cba512a2c7a2ad544ddb2533a90101fd1852b426e" "1297a022df4228b81bc0436230f211bad168a117282c20ddcba2db8c6a200743" default))))
+    ("51b8c4adab95ff23b8f5cf07ea0b9805c8662936fe0d877d61a0dd02b6adc5f6" "e80932ca56b0f109f8545576531d3fc79487ca35a9a9693b62bf30d6d08c9aaf" "cd540cb356cb169fa1493791bd4cbb183c5ad1c672b8d1be7b23e5e8c8178991" "06f0b439b62164c6f8f84fdda32b62fb50b6d00e8b01c2208e55543a6337433a" "4e262566c3d57706c70e403d440146a5440de056dfaeb3062f004da1711d83fc" "8db4b03b9ae654d4a57804286eb3e332725c84d7cdab38463cb6b97d5762ad26" "31a01668c84d03862a970c471edbd377b2430868eccf5e8a9aec6831f1a0908d" "f0a99f53cbf7b004ba0c1760aa14fd70f2eabafe4e62a2b3cf5cabae8203113b" "9dae95cdbed1505d45322ef8b5aa90ccb6cb59e0ff26fef0b8f411dfc416c552" "bb08c73af94ee74453c90422485b29e5643b73b05e8de029a6909af6a3fb3f58" "628278136f88aa1a151bb2d6c8a86bf2b7631fbea5f0f76cba2a0079cd910f7d" "1b8d67b43ff1723960eb5e0cba512a2c7a2ad544ddb2533a90101fd1852b426e" "1297a022df4228b81bc0436230f211bad168a117282c20ddcba2db8c6a200743" default)))
+ '(erc-autojoin-channels-alist (quote (("freenode.net" "#clojure" "#clojurescript"))))
+ '(erc-user-full-name "bryan maass")
+ '(helm-ag-fuzzy-match t)
+ '(package-selected-packages
+   (quote
+    (powerline zop-to-char zenburn-theme wsd-mode web-mode volatile-highlights vkill undo-tree smex smartrep smartparens scss-mode rainbow-mode rainbow-delimiters pixie-mode ov operate-on-number ocodo-svg-modelines nim-mode nginx-mode neotree mustache-mode move-text monokai-theme moe-theme markdown-mode magit less-css-mode json-mode js2-mode howdoi helm-projectile-all helm-projectile helm-google helm-descbinds helm-ag guru-mode guide-key grizzl god-mode gitignore-mode github-browse-file gitconfig-mode git-timemachine gist geiser flycheck flx-ido expand-region exec-path-from-shell emmet-mode elisp-slime-nav easy-kill django-mode discover-my-major diminish diff-hl darkroom darkburn-theme cyberpunk-theme csv-mode company-anaconda color-theme-solarized color-theme-sanityinc-tomorrow clojure-snippets clojure-quick-repls clojure-mode-extra-font-locking clojure-cheatsheet cljsbuild-mode cljr-helm cider-eval-sexp-fu browse-kill-ring anzu align-cljlet ace-window ace-jump-buffer ac-cider 4clojure 2048-game)))
+ '(powerline-default-separator (quote arrow))
+ '(volatile-highlights-mode t)
+ '(web-mode-markup-indent-offset 2))
+
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(vhl/default-face ((t (:inverse-video t)))))
--- personal.el
+++ personal.el
@@ -6,23 +6,24 @@
 ;; These are some personal settings that I find
 ;; _Extremely_ nessicary, and some personal functions.
 
-(setq inhibit-splash-screen 1)          ; no splash screen, thanks
-(line-number-mode 1)                    ; have line numbers and
-(column-number-mode 1)                  ; column numbers in the mode line
-(tool-bar-mode -1)                      ; no tool bar with icons
-(scroll-bar-mode -1)                    ; no scroll bars
-(global-linum-mode 1)                   ; add line numbers on the left
-(setq x-select-enable-clipboard 1)      ; Use the system clipboard
-(global-auto-revert-mode 1)             ; pickup external file changes (i.e. git)
-(setq-default sh-basic-offset 2)        ; shell
-(setq-default sh-indentation 2)         ; shell indentation
-(setq-default tab-width 2)              ; Tab width of 2
-(fset 'yes-or-no-p 'y-or-n-p)           ; enable y/n answers
-(setq initial-scratch-message "")       ; empty scratch message
+(setq inhibit-splash-screen 1)        ;; no splash screen, thanks
+(line-number-mode 1)                  ;; have line numbers and
+(column-number-mode 1)                ;; column numbers in the mode line
+(tool-bar-mode -1)                    ;; no tool bar with icons
+(scroll-bar-mode -1)                  ;; no scroll bars
+(global-linum-mode 1)                 ;; add line numbers on the left
+(setq select-enable-clipboard 1)      ;; Use the system clipboard
+(global-auto-revert-mode 1)           ;; pickup external file changes (i.e. git)
+(setq-default sh-basic-offset 2)      ;; shell
+(setq-default sh-indentation 2)       ;; shell indentation
+(setq-default tab-width 2)            ;; Tab width of 2
+(fset 'yes-or-no-p 'y-or-n-p)         ;; enable y/n answers
+(setq initial-scratch-message "")     ;; empty scratch message
 (setq ring-bell-function
-      (lambda () (message "*beep*")))   ; dont beep outloud, thats rude.
-(setq auto-window-vscroll nil)          ; better scrolling
-(global-hl-line-mode -1)                ; do not highlight line at point
+      (lambda () (message "*beep*"))) ;; dont beep outloud, thats rude.
+(setq auto-window-vscroll nil)        ;; better scrolling
+(global-hl-line-mode 1)               ;; do highlight line at point
+(setq prelude-flyspell nil)           ;; no more red boxes
 
 (setq backup-directory-alist
       `((".*" . ,temporary-file-directory)))
@@ -31,14 +32,13 @@
 
 ;; Navigate windows with shift-<arrows>
 (windmove-default-keybindings 'shift)
-(setq windmove-wrap-around -1)
+(setq windmove-wrap-around "no")
 
 ;; Helm settings:
 (helm-autoresize-mode 1)
 
-
+;; on mac, there's always a menu bar drawn, don't have it empty
 (unless (string-match "apple-darwin" system-configuration)
-  ;; on mac, there's always a menu bar drawn, don't have it empty
   (menu-bar-mode -1))
 
 ;; under mac, have Command as Super and Alt as Meta
@@ -50,28 +50,31 @@
 ;; Save here instead of littering current directory with emacs backup files
 (setq backup-directory-alist `(("." . "~/.saves")))
 
+
 ;; Font size
 (define-key global-map (kbd "C-+") 'text-scale-increase)
 (define-key global-map (kbd "C--") 'text-scale-decrease)
 
 
-
-(defun save-macro (name)
-  ;; Save a macro.
-;; Take a name as argument
-;; and save the last defined macro under
-;; this name at the end of your user.el
-  (interactive "Same of the macro: ")
-  (kmacro-name-last-macro name)
-  (find-file "~/.emacs.d/personal/saved-macros.el") ;; user-init-file
-  (goto-char (point-max))
-  (newline)
-  (insert-kbd-macro name)
-  (newline)
-  (switch-to-buffer nil))
-
-
 ;; multiple cursor mode defaults.
+
+(defun stand ()
+  (interactive)
+  (find-file
+   (concat "~/notes/standup"
+           (format-time-string "%Y-%m-%d")
+           ".org")))
+
+(defun fetchh-notes ()
+  (interactive)
+  (find-file "~/notes/fetchh.org"))
+
+(defun todo ()
+  (interactive)
+  (find-file "~/fetchh/fetchh_sync/todo/bryan.md"))
+
+(global-set-key [f9] 'todo)
+
 (global-set-key (kbd "C->") 'mc/mark-next-like-this)
 (global-set-key (kbd "C-<") 'mc/mark-previous-like-this)
 (global-set-key (kbd "C-c C-<") 'mc/mark-all-like-this)
@@ -81,8 +84,48 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 ;; use prelude to auto-install whatever the fuck packages I want.
+
+(smartparens-global-mode 1)
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;  use prelude to auto-install whatever the fuck packages I want.
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
 (add-to-list 'prelude-packages 'clj-refactor)
+(add-to-list 'prelude-packages 'emmet-mode)
+(add-to-list 'prelude-packages 'wsd-mode)
+(add-to-list 'prelude-packages 'github-browse-file)
+(add-to-list 'prelude-packages 'guide-key)
+(add-to-list 'prelude-packages 'darkburn-theme)
+(add-to-list 'prelude-packages 'neotree)
+(add-to-list 'prelude-packages 'align-cljlet)
+(add-to-list 'prelude-packages 'powerline)
+(add-to-list 'prelude-packages 'moe-theme)
+
 (prelude-install-packages)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;  END use prelude to auto-install
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(require 'powerline)
+(require 'moe-theme)
+(moe-dark)
+(powerline-moe-theme)
+
+(require'guide-key)
+(setq guide-key/guide-key-sequence '("C-x r" "C-x 4" "C-c" "C-h"))
+(setq guide-key/recursive-key-sequence-flag t)
+(setq guide-key/idle-delay 1.0)
+(guide-key-mode 1)  ; Enable guide-key-mode
+
+;; Auto-start on 'any markup' modes
+(add-hook 'sgml-mode-hook 'emmet-mode)
+(add-hook 'css-mode-hook  'emmet-mode)
+(add-hook 'web-mode-hook  'emmet-mode)
+
+(setq helm-display-header-line nil) ;; t by default
+(set-face-attribute 'helm-source-header nil :height 0.1)
+(setq helm-split-window-in-side-p 't)
 
 ;; set clj + clj refactor mode on clojure files
 (add-to-list 'auto-mode-alist '("\\.clj\\'" . clojure-mode))
@@ -92,10 +135,13 @@
 (require 'clj-refactor)
 (add-hook 'clojure-mode-hook (lambda ()
                                (clj-refactor-mode 1)
-                               (cljr-add-keybindings-with-prefix "C-c C-m")))
+                               (cljr-add-keybindings-with-prefix "C-c C-m")
+                               (local-set-key (kbd ";") 'sp-comment)))
 
+(global-set-key (kbd "C-c h s") 'helm-do-ag)
+(global-set-key (kbd "C-c C-s") 'helm-occur)
 
-
+(setq magit-last-seen-setup-instructions "1.4.0")
 
 (provide 'personal)
 ;;; personal.el ends here
